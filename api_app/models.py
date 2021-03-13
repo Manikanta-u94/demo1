9970fabf6dfef62288958c76a25962cee604a309
Manikanta <manikanta.u94@gmail.com>
2021-03-13 19:26:50 +0530

    latest

--- api_app/models.py
+++ api_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
@@ -6,7 +8,8 @@ class post(models.Model):
 	name = models.CharField(max_length = 50)
 	description = models.TextField()
 	user_id = models.IntegerField(default = 1)
-	#image = models.FileField(upload_to ='media',null='True',blank='True')
+	parent_id = models.IntegerField(default=11)
+	image = models.FileField(upload_to ='media',null='True',blank='True')
 
 	def __str__(self):
 		return self.name
--- api_app/views.py
+++ api_app/views.py
@@ -11,11 +11,18 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 # Create your views here.
 
 
 class PostViews(APIView):
+
+	filter_backends = [DjangoFilterBackend]
+
+	filterset_fields = ['name', ]
 
 
 	def get(self, request, format=None):
@@ -35,33 +42,30 @@ class postDetail(APIView):
 
 	parser_class = (FileUploadParser,)
 
-	# def get_object(self, pk):
-
-	# 	qs = post.objects.get(pk=pk)
-	# 	serializer = postserializer(qs)
-	# 	return Response(serializer.data)
-
 
 	def get(self, request, pk, format=None):
-	    post = self.get_object(pk)
-	    serializer = postserializer(post)
+	    qs = self.get_object(pk)
+	    serializer = postserializer(qs)
 	    return Response(serializer.data)
 
 	def put(self, request, pk, format=None):
-	    post = self.get_object(pk)
-	    serializer = postserializer(post, data=request.data)
+	    qs = self.get_object(pk)
+	    serializer = postserializer(qs, data=request.data)
 	    if serializer.is_valid():
 	        serializer.save()
 	        return Response(serializer.data)
 	    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 	def delete(self, request, pk, format=None):
-	    post = self.get_object(pk)
-	    post.delete()
+	    qs = self.get_object(pk)
+	    qs.delete()
 	    return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 
 
